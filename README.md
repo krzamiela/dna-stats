8db0e1b425ccb7e3aa79867b90de0a32267a45b0
kathrynzamiela <kathryn.zamiela@northwestern.edu>
2024-04-18 12:14:24 -0500

    Updated readme with usage

--- README.md
+++ README.md
@@ -1,2 +1,75 @@
 # dna-stats
-Final project for Coursera's Python for Genomics course, offered through Johns Hopkins
+Final project for Coursera's Python for Genomics course, offered through Johns Hopkins.
+Reads a multi-FASTA file and performs operations on extracted data.
+
+## Requirements
+- Python 3
+
+## Usage
+    pygenfinal.py [-h] <filename>
+
+    -h              print this message
+    
+    <filename>      the file has to be in FASTA format
+
+## Summary
+This program was developed as a final project, and parts of it could be improved for better usage.  For example, I chose to wrap all statistics about the given multi-FASTA file into a class.  I could have split the class into a separate module and provided an example Python file showing how to interface with the class.
+
+## SeqStats Class Basics
+To create a new instance:
+    foo = SeqStats(seq_dict)
+
+**Note:** The variable seq_dict must be formatted such that each key represents a sequence ID, and each value is a list of sequences matching that sequence ID
+
+Upon creation, the SeqStats class automatically calculates some statistics for the given seq_dict, which can be summarized by calling print_seq_stats():
+    foo.print_seq_stats()
+
+This prints:
+
+- Number of records
+- A dictionary of sequence lengths
+- The longest sequence length
+- The total number of sequences of the longest sequence length
+- A list of all sequences with the longest sequence length
+- The shortest sequence length
+- The total number of sequence of the shortest sequence length
+- A list of all sequences with the shortest sequence length
+
+To calculate ORF statistics, invoke the find_orfs() method:
+    foo.find_orfs(1)
+
+**Note:** find_orfs() expects an integer of either 1, 2, or 3, to specify the reading frame.
+
+To print ORF statistics based on the current reading frame, use print_orf_stats():
+    foo.print_orf_stats()
+
+This prints:
+
+- Length of the longest ORF in the file
+- Longest ORF sequence in the file
+- Sequence ID of longest ORF
+
+To calculate ORF statistics for a particular sequence ID only, enter that sequence ID as an optional second argument:
+    foo.get_longest_orf('bar')
+
+To print ORF statistics based on the current reading frame for a particular sequence ID only, enter that sequence ID as an optional second argument:
+    foo.print_orf_stats('bar')
+
+This prints:
+
+- Length of the longest ORF in the given sequence
+- Longest ORF in the given sequence
+- Starting position in sequence of the longest ORF
+
+To find all repeats of length n in the file, use get_repeats():
+    foo.get_repeats(n)
+
+**Note:** get_repeats() expects a positive integer value
+
+To print repeat statistics of length n, use print_repeat_stats():
+    foo.print_repeat_stats()
+
+This prints:
+
+- A dictionary of all repeats and their frequencies
+- A dictionary of the highest repeat frequency and all sequences with this frequency
---
+++ pygenfinal.py
@@ -0,0 +1,395 @@
+#!/usr/bin/python
+# Creator: Kathryn Zamiela
+# Description: Takes command-line input of a file containing DNA sequences in multi-FASTA format,
+#               extracts the data, and performs operations on the data
+# Questions for instructor: Do you ever run into sequences where you encounter a start codon and then another start codon, or a stop codon and then another stop codon?  Or are all DNA sequences start, stuff, stop?
+
+import sys
+import getopt
+
+
+# Input: none
+# Output: string with the file path
+def check_args():
+    o, a = getopt.getopt(sys.argv[1:], 'h')
+
+    opts = {}
+
+    for k, v in o:
+        opts[k] = v
+
+    # if user asked for help, print usage and exit
+    if '-h' in opts.keys():
+        usage(); sys.exit() # multiple instructions on same line if sep by ;
+
+    # check for requirements
+    if len(a) < 1:
+        usage(); sys.exit("***Error***: Input FASTA file is missing")
+    elif len(a) == 1:
+        # if the correct number of argument are present, return file name
+        return a[0] # only returning first element in the list since we know the list length is 1
+    else:
+        usage(); sys.exit("***Error***: Incorrect number of arguments provided")
+
+
+# Input: opened file
+# Output: dictionary of sequences
+def create_sequence_dict(f):
+    seq_dict = {}
+
+    for line in f:
+        # discard newlines
+        line = line.rstrip()
+
+        if line[0] == '>': # if header
+            # split on whitespace
+            words = line.split()
+
+            # name is after the >
+            name = words[0][1:]
+
+            # create new entry in dict
+            seq_dict[name] = ''
+        else: # not header
+            # update seq in dict
+            seq_dict[name] = seq_dict[name] + line
+
+    # check if there are any sequences in the dict
+    if len(seq_dict.keys()) <= 0:
+        sys.exit("***Error***: No DNA sequences found in file.  Please ensure that your file is in FASTA format.")
+    else:
+        return seq_dict
+    
+
+# Input: a string path to a file containing DNA sequences in multi-FASTA format
+# Output: file handler
+def open_file(filename):
+
+    try:
+        f = open(filename)
+    except IOError:
+        print(filename); sys.exit("File does not exist")
+
+    return f
+
+
+# Input: none
+# Output: text of how to use the file
+def usage():
+    print("""
+    pygenfinal.py : reads a FASTA file and performs operations on extracted data
+
+    pygenfinal.py [-h] <filename>
+
+    -h              print this message
+    
+    <filename>      the file has to be in FASTA format
+    """)
+
+
+# Class to store the sequence data and calculate statistics about it
+# Takes a dictionary of 
+class SeqStats:
+    # on initialization, calculate metrics
+    def __init__(self, seq_dict):
+        self.data = seq_dict
+        self.seq_len_dict = self.len_sequences()
+        self.longest_seq_len = 0
+        self.get_longest_seq()
+        self.longest_seq_list = []
+        self.create_longest_seq_list()
+        self.shortest_seq_len = 0
+        self.get_shortest_seq()
+        self.shortest_seq_list = []
+        self.create_shortest_seq_list()
+        self.start_codon = 'ATG'
+        self.stop_codons = ['TAA', 'TAG', 'TGA']
+        self.orf_dict = {}
+        self.longest_orf_length = 0
+        self.longest_orf = 0
+        self.longest_orf_seq_id = 0
+        self.longest_orf_seq_length = 0
+        self.longest_orf_seq = 0
+        self.longest_orf_seq_start = -1
+        self.repeat_dict = {}
+        self.highest_repeat_dict = {}
+
+
+    # get number of records
+    def num_records(self):
+        return len(self.data.keys())
+
+
+    # create dictionary to store sequence lengths
+    def len_sequences(self):
+        len_dict = {}
+
+        for key in self.data.keys():
+            len_dict[key] = len(self.data[key])
+
+        # return dictionary
+        return len_dict
+
+
+    # get longest sequence in file
+    def get_longest_seq(self):
+        # find
+        for key in self.seq_len_dict.keys():
+            # if sequence length greater than stored sequence length
+            if self.seq_len_dict[key] > self.longest_seq_len:
+                # save max sequence length
+                self.longest_seq_len = self.seq_len_dict[key]
+
+
+    # save all keys with this longest sequence length
+    def create_longest_seq_list(self):
+        for key in self.seq_len_dict.keys():
+            # if length is max seq length
+            if self.seq_len_dict[key] == self.longest_seq_len:
+                self.longest_seq_list.append(key)
+
+
+    # get shortest sequence in file
+    def get_shortest_seq(self):
+        # find shortest seq
+        for key in self.seq_len_dict.keys():
+            # if a length hasn't been defined yet, use the current length as a base
+            if self.shortest_seq_len == 0:
+                self.shortest_seq_len = self.seq_len_dict[key]
+            elif self.seq_len_dict[key] < self.shortest_seq_len:
+                # if sequence length smaller than stored sequence length
+                # save shortest sequence length
+                self.shortest_seq_len = self.seq_len_dict[key]
+
+
+    # save all keys with this shortest sequence length
+    def create_shortest_seq_list(self):
+        for key in self.seq_len_dict.keys():
+            if self.seq_len_dict[key] == self.shortest_seq_len:
+                self.shortest_seq_list.append(key)
+
+
+    # find all ORFs
+    def find_orfs(self, reading_frame):
+        # if reading frame is a weird value, print usage and end
+        if reading_frame < 1 or reading_frame > 3:
+            print("Please enter a reading frame of 1, 2, or 3")
+            return
+
+        for seq_id, seq in self.data.items():
+            start_idx = reading_frame - 1
+
+            # start blank list of strings
+            self.orf_dict[seq_id] = []
+
+            for i in range(start_idx, (len(seq) - 2), 3):
+                codon = seq[i:i + 3]
+
+                # if start codon, store
+                if codon == self.start_codon:
+                    exon = codon
+
+                    # advance i
+                    i = i + 3
+
+                    # get the rest of the sequence
+                    for j in range(i, (len(seq) - 2), 3):
+                        # Question for instructor: do we need to handle a start codon in a sequence without a stop codon?  Does this ever occur?
+                        codon = seq[j:j + 3]
+                        
+                        # add codon
+                        exon = exon + codon
+
+                        # if stop codon, break
+                        if codon in self.stop_codons:                            
+                            # save string to list
+                            self.orf_dict[seq_id].append(exon)
+
+                            # increment read index
+                            i = j + 3
+
+                            # exit loop
+                            break
+
+        # get longest ORF stats for entire file
+        self.get_longest_orf()
+
+
+    # get the starting position in the sequence string of a sub-sequence
+    def get_subseq_start_pos(self, seq_id, seq):
+        # get full sequence from seq_id
+        full_seq = self.data[seq_id]
+
+        # find subseq in seq
+        start_pos = full_seq.find(seq)
+
+        # if sequence not found, print error but still return
+        if start_pos == -1:
+            print("Sequence ", seq, " not found for sequence ID: ", seq_id)
+
+        # return starting position (we want character position, not index)
+        return start_pos + 1
+
+
+    # get longest ORF in file
+    def get_longest_orf(self, seq_id=0):
+        # if given seq id, calculate ORF metrics for that sequence only
+        if seq_id != 0:
+            seq_list = self.orf_dict[seq_id]
+
+            for seq in seq_list:
+                # if current seq longer than max seq, update longest_orf_seq
+                if len(seq) > self.longest_orf_seq_length:
+                    self.longest_orf_seq_length = len(seq)
+                    self.longest_orf_seq = seq
+                    # find starting character in original sequence
+                    self.longest_orf_seq_start = self.get_subseq_start_pos(seq_id, seq)
+        else: # calculate ORF metrics for entire file
+            # iterate through orf dict list
+            for key in self.orf_dict.keys():
+                seq_list = self.orf_dict[key]
+
+                for seq in seq_list:
+                    # if current seq longer than max seq, update longest_orf and note position
+                    if len(seq) > self.longest_orf_length:
+                        self.longest_orf_length = len(seq)
+                        self.longest_orf = seq
+                        self.longest_orf_seq_id = key
+
+    
+    # get all repeats (overlapping) of length n
+    # calculate how many times each repeat occurs in the file
+    def get_repeats(self, n):
+        # get DNA sequence
+        for key in self.data.keys():
+            seq = self.data[key]
+
+            # iterate through sequence
+            for i in range(0, (len(seq) - n)):
+                subseq = seq[i:i+n]
+
+                # if sub-sequence is already a known repeat, increment counter
+                if subseq in self.repeat_dict.keys():
+                    self.repeat_dict[subseq] = self.repeat_dict[subseq] + 1
+                else:
+                    # add subsequence to dict
+                    self.repeat_dict[subseq] = 1
+
+        # delete all dictionary entries where repeat count is 1
+        new_rep_dict = { key:value for key, value in self.repeat_dict.items() if value > 1 }
+        self.repeat_dict = new_rep_dict
+
+        # calculate highest repeat frequency
+        self.get_highest_repeats()
+
+
+    # most frequent repeat of a given length
+    def get_highest_repeats(self):
+        max_repeat_freq = 0
+
+        for key in self.repeat_dict.keys():
+            # if repeat frequency greater than stored repeat frequency
+            if self.repeat_dict[key] > max_repeat_freq:
+                # update max repeat frequency
+                max_repeat_freq = self.repeat_dict[key]
+
+        # save all repeats of max frequency in a dictionary
+        for key in self.repeat_dict.keys():
+            if self.repeat_dict[key] == max_repeat_freq:
+                self.highest_repeat_dict[key] = self.repeat_dict[key]
+
+
+    # print stats
+    def print_seq_stats(self):
+        # number of records
+        print("Number of records: ", self.num_records())
+        # all sequence lengths
+        print("Sequence lengths: ", self.seq_len_dict)
+        # longest sequence length
+        print("Longest sequence length: ", self.longest_seq_len)
+        # seqs with max length
+        print("Total sequences of longest length: ", len(self.longest_seq_list))
+        # keys of seqs with max length
+        print("Longest sequences: ", self.longest_seq_list)
+        # shortest sequence length
+        print("Shortest sequence length: ", self.shortest_seq_len)
+        # seqs with min length
+        print("Total sequences of shortest length: ", len(self.shortest_seq_list))
+        # keys of seqs with min length
+        print("Shortest sequences: ", self.shortest_seq_list)
+
+
+    # print ORF-specific stats
+    def print_orf_stats(self, seq_id=0):
+        # if only self is entered, assume stats for all ORFs in file
+        if seq_id == 0:
+            # longest ORF length
+            print("Longest ORF in file: ", self.longest_orf_length, " characters")
+            # longest ORF
+            print("Longest ORF sequence in file: ", self.longest_orf)
+            # longest ORF seq ID
+            print("Sequence ID of longest ORF: ", self.longest_orf_seq_id)
+        else: # print stats for specific sequence ID only (must have already been created)
+            # longest ORF in seq length
+            print("Longest ORF in sequence ", seq_id, ": ", self.longest_orf_seq_length, " characters")
+            # longest ORF in seq
+            print("Longest ORF in sequence ", seq_id, ": ", self.longest_orf_seq)
+            # longest ORF starting position
+            print("Longest ORF starting position in sequence  ", seq_id, ": ", self.longest_orf_seq_start)
+
+
+    # print repeat-specific stats
+    def print_repeat_stats(self):
+        # all repeat frequencies of given length
+        print("All repeat frequencies: ", self.repeat_dict)
+        # highest repeat frequencies
+        print("Highest repeat frequencies: ", self.highest_repeat_dict)
+
+
+## MAIN ##
+# check if arguments provided are valid
+fname = check_args()
+
+# open file
+ufile = open_file(fname)
+
+# create dictionary of sequences
+seqs = create_sequence_dict(ufile)
+
+# create SeqStats class and get metrics
+all_stats = SeqStats(seqs)
+
+# print stats
+all_stats.print_seq_stats()
+
+# identify all ORFs
+# prompt for reading frame
+reading_frame = 0
+
+while reading_frame not in ['1', '2', '3']:
+    reading_frame = input("At which index would you like to start the reading frame: 1, 2, or 3? ")
+
+# what is the longest ORF in the file? what is the identifier of the containing sequence?
+all_stats.find_orfs(int(reading_frame))
+all_stats.print_orf_stats()
+
+# For a given sequence identifier, what is the longest ORF contained in the sequence?
+orf_seq = ''
+while orf_seq != 'y' and orf_seq != 'n':
+    orf_seq = input("Do you want to calculate metrics for the longest ORF of a particular sequence? (y/n) ")
+
+if orf_seq == 'y':
+    test_seq_id = input("Please input the sequence ID you would like to test: ")
+
+    all_stats.get_longest_orf(test_seq_id)
+    all_stats.print_orf_stats(test_seq_id)
+
+n = ""
+
+while not n.isnumeric():
+    n = input("Please enter repeat length as an integer: ")
+
+# Given a length n, identify all repeats of length n in all sequences in the FASTA file
+all_stats.get_repeats(int(n))
+
+all_stats.print_repeat_stats()
